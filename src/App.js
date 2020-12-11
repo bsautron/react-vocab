c51cb7cdbbe1b4133c6c4bd388f0484223259249
Bruno Sautron <sautron.brunojj@gmail.com>
2020-12-11 13:27:23 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import logo from './logo.svg'
+
+import './App.css'
+import PageRevisions from './pages/revision'
+
+function App() {
+  return <PageRevisions></PageRevisions>
+}
+
+export default App
---
+++ src/pages/revision.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PageRevisions () {
+	return (
+		<div>houla</div>
+	);
+}
+
+export default PageRevisions;
